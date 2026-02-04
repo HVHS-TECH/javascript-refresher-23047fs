5a3c2215dd02404a7bee071e921c7ceaf7b76dab
23047fs <23047fs@hvhs.school.nz>
2026-02-05 10:34:08 +1300

    save

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 let header;
-let head;
 
 function headerButton() {
     header=document.getElementById("test")
-    header=head
     console.log("headerButton")
 }
 
 function alertButton() {
     //alert("Congratulations!\nYou got the script running.")
-    document.getElementById("welcomeMessage").innerHTML=head
+    document.getElementById("welcomeMessage").innerHTML=header
     console.log("alertButton")
 }
