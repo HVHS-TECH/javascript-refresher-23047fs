169734a29b6be5068fc5ede2cc650a431757557d
23047fs <23047fs@hvhs.school.nz>
2026-02-05 10:15:37 +1300

    save

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+let test=1;
 
 function alertButton() {
-    alert("Congratulations!\nYou got the script running.")
-    document.getElementById("welcomeMessage").innerHTML="Test"
+    //alert("Congratulations!\nYou got the script running.")
+    document.getElementById("welcomeMessage").innerHTML=test
 }
